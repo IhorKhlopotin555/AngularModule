222b3f28ec83dceac0eebb7b4f3edbcf86c9ec1e
IhorKhlopotin555 <hlopotinigor555@gmail.com>
2022-07-17 15:57:27 +0300

    lesson2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,27 @@ import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import { UsersComponent } from './components/users/users.component';
 import { UserComponent } from './components/user/user.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    UserComponent
+    UserComponent,
+    UserDetailsComponent
   ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        RouterModule.forRoot([
+          {path: 'users-page', component: UsersComponent,
+            children: [
+              {path:'user-details/:id', component: UserDetailsComponent}
+            ]}
+
+        ])
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/models/IUser.ts
+++ src/app/models/IUser.ts
@@ -1,6 +1,9 @@
+import {ICompany} from "./iCompany";
+
 export interface IUser{
   id:number;
   name:string;
   username:string;
   email:string;
+  company: ICompany
 }
