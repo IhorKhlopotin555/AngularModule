9c30eb30fa090d6836eb8b4186f457ffda54770b
IhorKhlopotin555 <hlopotinigor555@gmail.com>
2022-07-19 12:17:00 +0300

    5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
+import { LessonSixComponent } from './lesson-six/lesson-six.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    LessonSixComponent
   ],
   imports: [
     BrowserModule
