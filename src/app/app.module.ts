a5b993051bcead8bb407ee223395394b0dce5b2a
IhorKhlopotin555 <hlopotinigor555@gmail.com>
2022-07-19 09:35:54 +0300

    lesson5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,31 +2,41 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {HttpClientModule} from "@angular/common/http";
-import { UsersComponent } from './components/users/users.component';
-import { UserComponent } from './components/user/user.component';
-import { UserDetailsComponent } from './components/user-details/user-details.component';
-import {RouterModule} from "@angular/router";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { CarsComponent } from './components/cars/cars.component';
+import { CarComponent } from './components/car/car.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HeaderComponent } from './components/header/header.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MainInterceptor} from "./main.interceptor";
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
-    UserComponent,
-    UserDetailsComponent
+    MainLayoutComponent,
+    CarsComponent,
+    CarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HeaderComponent
   ],
-    imports: [
-        BrowserModule,
-        HttpClientModule,
-        RouterModule.forRoot([
-          {path: 'users-page', component: UsersComponent,
-            children: [
-              {path:'user-details/:id', component: UserDetailsComponent}
-            ]}
-
-        ])
-    ],
-  providers: [],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    {
+      provide:HTTP_INTERCEPTORS,
+      multi:true,
+      useClass: MainInterceptor
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/models/IUser.ts
+++
@@ -1,9 +0,0 @@
-import {ICompany} from "./iCompany";
-
-export interface IUser{
-  id:number;
-  name:string;
-  username:string;
-  email:string;
-  company: ICompany
-}
